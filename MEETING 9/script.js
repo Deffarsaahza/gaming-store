2c4408b0a296409f4f179190f5932bc11a6dd5b0
Deffarsaahza <166132269+Deffarsaahza@users.noreply.github.com>
2024-05-17 19:47:55 +0700

    update

---
+++ MEETING 9/script.js
@@ -0,0 +1,28 @@
+// var result = document.getElementById("programmer").innerHTML
+// console.log(result) //tampilkan di log
+
+// var result = document.getElementsByTagName("p")[1].innerHTML
+// console.log(result) //tampilkan di log
+
+// var result = document.getElementsByClassName("apps")[0].innerHTML
+// console.log(result) //tampilkan di log
+
+// var item = document.getElementsByClassName("apps")
+// var i;
+// for (i=0; i< item.length;i++ ) {
+//  var results = item[i].innerHTML;
+//  console.log(result)}
+
+ var result = document.getElementsByTagName("p")[4].innerHTML = "Hello World";
+
+var result = document.getElementsByClassName("red")[0].className = "red";
+
+var result = document.getElementsByClassName("red")[0].style.color = "red";
+
+  var result = document.getElementsByTagName("h2")[0].setAttribute("class", "blue")
+
+//  var paragraph = document.createElement("P");
+// paragraph.innerHTML = "New Paragraph";
+
+// document.body.appendChild(paragraph);
+// document.getElementById("programmer").element.style.backgroundColor = "red";
