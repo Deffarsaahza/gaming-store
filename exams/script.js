1f6d875217b9c9ccd3faa497d0ae7dec6c462154
Deffarsaahza <166132269+Deffarsaahza@users.noreply.github.com>
2024-06-21 19:38:51 +0700

    update

---
+++ exams/script.js
@@ -0,0 +1,23 @@
+function changeImage(element) {
+    element.setAttribute("src","img-header2.png")
+   }
+   function changeImageBack(element) {
+    element.setAttribute("src","img-header1.png")
+   }
+
+   var btnLike = document.querySelector("#green")
+   var btnDislike = document.querySelector("#red")
+   btnLike.onclick = likeColor
+   btnDislike.onclick = dislikeColor
+   function likeColor() {
+    if (btnDislike.classList.contains("red")) {
+    btnDislike.classList.remove("red")
+    }
+    this.classList.toggle("green")
+   }
+   function dislikeColor() {
+    if (btnLike.classList.contains("green")) {
+    btnLike.classList.remove("green")
+    }
+    this.classList.toggle("red")
+   }
